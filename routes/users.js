ee0b5f4273e366e43b5e8076efddc19abb3a99d3
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2024-10-23 21:53:45 +0800

    initial commit - installed redis, websocket etc

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+let users = [{
+  id: Math.floor(Math.random * 1000),
+  name: `Alex ${this.id}`,
+}];
+
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+  users.push(req.body);
+  res.send('new user was created')
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(users);
+});
+
+module.exports = router;
